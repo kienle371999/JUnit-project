f70a293901bfa147e9e6312c969aad30ce58b7f2
thanhphuonghdhh <19021346@vnu.edu.vn>
2022-03-02 00:20:54 +0700

    Update TestCalculation.java

--- tests/TestCalculation.java
+++ tests/TestCalculation.java
@@ -26,12 +26,29 @@ public class TestCalculation {
     }
 
     @Test
-    public void testAdd2() {
+    public void testDivide() {
         try {
             Assert.assertEquals(Long.valueOf(2L), calculation.divide(6L, 0L));
         }
         catch (Exception e) {
             Assert.assertEquals("Invalid Division", e.getMessage());
         }
+    }
+
+    @Test
+    public void testSubtract() {
+        Assert.assertEquals(Long.valueOf(1L), calculation.subtract(4L, 3L));
+    }
+
+    @Test
+    public void testMultiply() {
+        Assert.assertEquals(Long.valueOf(12L), calculation.multiply(4L, 3L));
+    }
+
+
+    @Test
+    public void testFindMaximumNumber() {
+        ArrayList<Long> arr = new ArrayList<Long>(Arrays.asList(1L, 5L, 2L, 10L));
+        Assert.assertEquals(Long.valueOf(10L), calculation.findMaximumNumber(arr);
     }
 }
